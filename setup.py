cc0221bba5b4727b1698edb25389ea9a75b4c871
Joseph Yusufov <jyusufov00@stuy.edu>
2020-01-25 21:51:12 -0500

    pulled from API, made into dict, printed values

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-import ryan
-import daniel
-import mamunur
-import muntasir
+import urllib3
+import json
+import random
+import csv
+import sqlite3
+http = urllib3.PoolManager()
+	
+r = http.request( 'GET', 
+	'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=NUOQ0ZH9OLWO0Y0S'
+	)
+r.status
+
+MSFT = json.loads(r.data.decode('utf-8'))
+
+# print(json.dumps(MSFT, indent=2))
+
+
+for member in MSFT["Time Series (5min)"]:
+	print("{}    {}    {}".format(member, MSFT["Time Series (5min)"][member]["2. high"], MSFT["Time Series (5min)"][member]["3. low"]))
