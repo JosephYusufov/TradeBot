2e00b0bb5df19976747efa3a0e7d9b67058f7c2c
JosephYusufov <jyusufov00@stuy.edu>
2020-01-28 17:06:10 -0500

    stupid stuff

--- alpaca.py
+++ alpaca.py
@@ -6,5 +6,21 @@ import sqlite3
 ALPACA_KEY = ""
 with open("credentials/alpaca_KEY.txt", 'r') as file:
     data = file.read()
-    ALPHAVANTAGE_KEY = data
+    ALPACA_KEY = data
+    
         
+def buy(symbol, qty):
+    """
+    Buys a quantity of shares of a stock.
+
+    Params:
+    symbol -- valid NYSE symbol
+    qty -- number of shares to buy
+    """
+    http = urllib3.PoolManager()
+    querystring = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={}&interval={}&apikey={}".format(
+        symbol, interval, ALPHAVANTAGE_KEY)
+    
+    # print(querystring)
+    r = http.request('POST', querystring, fields = {}) 
+    data = json.loads(r.data.decode('utf-8'))
