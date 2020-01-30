9f92671490cee6be48f3f5cf2afc8ba436837b63
Joseph Yusufov <jyusufov00@stuy.edu>
2020-01-29 23:09:01 -0500

    made simple script that submits a buy order and then immediately cancels usind SDK

--- strategies.py
+++ strategies.py
@@ -1,8 +1,19 @@
 import alpaca_trade_api as tradeapi
-API_KEY = "PKL38AOZCYBQTOLJQRWL"
-API_SECRET = "XpoxrGiRoc8DQOWgNaRmZASmRjE1gYBGcz2/SZ4S"
+API_KEY = "PKG1XYYRC92MYVF3B5U0"
+API_SECRET = "KembGvdw3Gflnrh5UJVqiwdHS1cz1IfgIJzfbwsJ"
 APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
 
-alpaca = tradeapi.REST(API_KEY, API_SECRET, api_version='v2') 
+alpaca = tradeapi.REST(API_KEY, API_SECRET, APCA_API_BASE_URL, api_version='v2') 
 # account = alpaca.get_account()
-print(alpaca.list_positions())
+# print(account)
+buy = alpaca.submit_order(
+    "ES",
+    "1",
+    "buy",
+    "market",
+    "day"
+)
+
+print(buy)
+
+cancel = alpaca.cancel_order(buy.id)
